2ee0bfbc998091eacda06daeaaaf7b56946e4e9f
Aditi Jain <aditijain.ind@gmail.com>
2022-01-26 13:42:20 -0600

    Update system.cpp
    Review Comments

--- src/system.cpp
+++ src/system.cpp
@@ -10,10 +10,7 @@
 #include "linux_parser.h"
 #include "format.h"
 
-using std::set;
-using std::size_t;
-using std::string;
-using std::vector;
+using namespace std;
 /*You need to complete the mentioned TODOs in order to satisfy the rubric criteria "The student will be able to extract and display basic data about the system."
 
 You need to properly format the uptime. Refer to the comments mentioned in format. cpp for formatting the uptime.*/
@@ -26,16 +23,16 @@ vector<Process>& System::Processes() {
     vector<int> pids = LinuxParser::Pids();
     processes_.clear();
     for(int p: pids){
-        processes_.push_back(Process(p));
+        processes_.emplace_back(p);
     }
     
-    std::sort(processes_.rbegin(), processes_.rend());
+    sort(processes_.rbegin(), processes_.rend());
 
     return processes_; 
 }
 
 // TODO(Done): Return the system's kernel identifier (string)
-std::string System::Kernel() { 
+string System::Kernel() { 
     return LinuxParser::Kernel(); 
 }
 
@@ -45,7 +42,7 @@ float System::MemoryUtilization() {
 }
 
 // TODO(Done): Return the operating system name
-std::string System::OperatingSystem() { 
+string System::OperatingSystem() { 
     return LinuxParser::OperatingSystem(); 
 }
 
